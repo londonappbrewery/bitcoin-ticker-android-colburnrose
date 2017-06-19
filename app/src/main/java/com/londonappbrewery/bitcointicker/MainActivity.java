468de6bf81b487e1fa83e3e74388098de4bd0ea2
colburnrose <colburnsanders@gmail.com>
2017-06-19 18:53:03 -0500

    updated gradle with http client dependency and set up spinner view

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -8,12 +8,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Toast;
-
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 
 
@@ -21,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/market/ticker/BTCUSD"; //per symbol ticker data.
 
     // Member Variables:
     TextView mPriceTextView;
@@ -45,7 +39,19 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Bitcoin", "" + parent.getItemAtPosition(position));
+                /*Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+                spinner.setOnItemSelectedListener(this);*/
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("Bitcoin", "Nothing selected!");
+            }
+        });
     }
 
     // TODO: complete the letsDoSomeNetworking() method
