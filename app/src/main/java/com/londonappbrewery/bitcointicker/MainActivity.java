4ed25af8ba611b8f4a49cb3400f833ce47d6bf76
colburnrose <colburnsanders@gmail.com>
2017-06-21 10:27:07 -0500

    updated the api call for each different currency in the spinner

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -15,7 +15,20 @@ public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"; //per symbol ticker data
+    //private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"; //per symbol ticker data
+    private final String AUD_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCAUD";
+    private final String BRL_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCBRL";
+    private final String CAD_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCCAD";
+    private final String CNY_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCCNY";
+    private final String EUR_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCEUR";
+    private final String GPB_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCGPB";
+    private final String HKD_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCHKD";
+    private final String JPY_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCJPY";
+    private final String PLN_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCPLN";
+    private final String RUB_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCRUB";
+    private final String SEK_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCSEK";
+    private final String USD_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD";
+    private final String ZAR_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCZAR";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -43,8 +56,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Log.d("Bitcoin", "" + parent.getItemAtPosition(position));
-                Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
-                spinner.setOnItemSelectedListener(this);
+
             }
 
             @Override
